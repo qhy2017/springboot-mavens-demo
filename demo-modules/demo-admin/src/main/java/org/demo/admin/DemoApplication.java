0e46182875e4f276fbed710f2aae9f529d660300
qhy2017 <1242359237@qq.com>
2018-10-14 12:07:39 +0800

    修改打包

--- demo-modules/demo-admin/src/main/java/org/demo/admin/DemoApplication.java
+++ demo-modules/demo-admin/src/main/java/org/demo/admin/DemoApplication.java
@@ -4,7 +4,6 @@ import org.demo.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.test.annotation.Repeat;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
