ac3c456c2824a58a7c7e3c178a3f8ea1baa0107f
qhy2017 <1242359237@qq.com>
2018-10-14 11:19:14 +0800

    修改包结构

---
+++ demo-modules/demo-admin/src/main/java/org/demo/admin/App.java
@@ -0,0 +1,13 @@
+package org.demo.admin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
